ec4061f03dc59af46101beadeeefd7175781322a
leonel911 <leonelbasilio911@gmail.com>
2022-04-16 16:22:57 -0300

    .Making Client CRUD;

--- src/main/java/com/loja/virtual/domain/Cliente.java
+++ src/main/java/com/loja/virtual/domain/Cliente.java
@@ -1,4 +1,113 @@
 package com.loja.virtual.domain;
 
-public class Cliente {
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "CLIENTE")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty(message = ("O nome é obrigatório."))
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres.")
+    private String nome;
+    @NotEmpty(message = ("Data de nascimento é obrigatório."))
+    private Date dataNasc;
+    @NotEmpty(message = ("Email é obrigatório."))
+    private String email;
+    private List<Endereco> enderecos;
+    @NotEmpty(message = ("Senha é obrigatório."))
+    private String senha;
+    private List<Pedido> pedidos;
+
+
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nome, Date dataNasc, String email, List<Endereco> enderecos, String senha, List<Pedido> pedidos) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.email = email;
+        this.enderecos = enderecos;
+        this.senha = senha;
+        this.pedidos = pedidos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return id.equals(cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
